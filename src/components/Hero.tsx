3a40370c24a65f8c6d401de590356efa89880daa
Malin Nilsson <malinnilsson87@gmail.com>
2022-06-25 21:01:50 +0200

    Added framer motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,32 +1,40 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import React from 'react'
 
 export default function Hero() {
   return (
-    <section className="hero-container">
 
-      <div className="video-container">
-        <video src="/videos/hero-video.mov" className="video-bg" autoPlay loop muted></video>
-      </div>
+    <AnimatePresence>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ opacity: 0.2 }}
+        className="hero-container">
 
-      <div className="hero-text"><h2>Adventure awaits</h2>
-        <div className="hero-btns">
-          <div className="hero-btn">
-            <button className="solid">Plan your journey</button>
-          </div>
-          <div className="hero-btn">
-            <button className="solid">
-              <span>Watch trailer</span>
-              <span><svg xmlns="http://www.w3.org/2000/svg"
-                width="16" height="16" fill="#fff"
-                viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
-              </svg></span>
-            </button>
+        <div className="video-container">
+          <video src="/videos/hero-video.mov" className="video-bg" autoPlay loop muted></video>
+        </div>
+
+        <div className="hero-text"><h2>Adventure awaits</h2>
+          <div className="hero-btns">
+            <div className="hero-btn">
+              <button className="solid">Plan your journey</button>
+            </div>
+            <div className="hero-btn">
+              <button className="solid">
+                <span>Watch trailer</span>
+                <span><svg xmlns="http://www.w3.org/2000/svg"
+                  width="16" height="16" fill="#fff"
+                  viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+                </svg></span>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-
-    </section>
+      </motion.section>
+    </AnimatePresence>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Navbar() {
     const [isActive, setIsActive] = useState<Boolean>(false);
@@ -10,76 +11,90 @@ export default function Navbar() {
 
     return (
         <>
-            <nav>
-                <div className="nav-container">
-                    <header>
-                        <h1>
-                            <NavLink to="/">
-                                TRVL
-                                <i className='fab fa-typo3' />
-                            </NavLink>
-                        </h1>
+            <AnimatePresence>
+                <motion.nav
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ opacity: 0.3 }}>
+                    <div className="nav-container">
+                        <header>
+                            <h1>
+                                <NavLink to="/">
+                                    TRVL
+                                    <i className='fab fa-typo3' />
+                                </NavLink>
+                            </h1>
 
-                        <span
-                            onClick={toggleMobileMenu}
-                            id="mobile-menu-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                width="40"
-                                height="40"
-                                fill="currentColor"
-                                viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-                            </svg>
-                        </span>
-                    </header>
+                            <span
+                                onClick={toggleMobileMenu}
+                                id="mobile-menu-icon">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    width="40"
+                                    height="40"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                                </svg>
+                            </span>
+                        </header>
 
-                    <ul style={{
-                        display: isActive && "flex",
-                        flexDirection: isActive && "column",
-                        gap: isActive && "15px",
-                        marginTop: isActive && "15px"
-                    }}
-                        className={isActive ? "mobile-menu" : ""}>
-                        <li className="nav-item hover-effect">
-                            <NavLink to="/destinations"
-                                onClick={() => isActive ? toggleMobileMenu() : ""}
-                                className={({ isActive }) => isActive ? "underline" : ""}>
-                                Destinations
-                            </NavLink>
-                        </li>
-                        <li className="nav-item hover-effect">
-                            <NavLink to="/experiences"
-                                onClick={() => isActive ? toggleMobileMenu() : ""}
-                                className={({ isActive }) => isActive ? "underline" : ""}>
-                                Tailored Experiences
-                            </NavLink>
-                        </li>
-                        <li className="nav-item hover-effect">
-                            <NavLink to="/rentals"
-                                onClick={() => isActive ? toggleMobileMenu() : ""}
-                                className={({ isActive }) => isActive ? "underline" : ""}>
-                                Private Rentals
-                            </NavLink>
-                        </li>
-                        <li className="nav-item hover-effect">
-                            <NavLink to="/about"
-                                onClick={() => isActive ? toggleMobileMenu() : ""}
-                                className={({ isActive }) => isActive ? "underline" : ""}>
-                                About us
-                            </NavLink>
-                        </li>
-                    </ul>
 
-                    <div className="navbar-icons">
-                        <div className="navbar-icon">
-                            <img src="/images/search.svg" alt="" />
-                        </div>
-                        <div className="navbar-icon">
-                            <img src="/images/heart.svg" alt="" />
+                        <motion.ul
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            transition={{ opacity: 0.3 }}
+                            style={{
+                                display: isActive && "flex",
+                                flexDirection: isActive && "column",
+                                gap: isActive && "15px",
+                                marginTop: isActive && "15px",
+                            }}
+                            className={isActive ? "mobile-menu" : ""}
+                        >
+                            <li className="nav-item hover-effect">
+                                <NavLink to="/destinations"
+                                    onClick={() => isActive ? toggleMobileMenu() : ""}
+                                    className={({ isActive }) => isActive ? "underline" : ""}>
+                                    Destinations
+                                </NavLink>
+                            </li>
+                            <li className="nav-item hover-effect">
+                                <NavLink to="/experiences"
+                                    onClick={() => isActive ? toggleMobileMenu() : ""}
+                                    className={({ isActive }) => isActive ? "underline" : ""}>
+                                    Tailored Experiences
+                                </NavLink>
+                            </li>
+                            <li className="nav-item hover-effect">
+                                <NavLink to="/rentals"
+                                    onClick={() => isActive ? toggleMobileMenu() : ""}
+                                    className={({ isActive }) => isActive ? "underline" : ""}>
+                                    Private Rentals
+                                </NavLink>
+                            </li>
+                            <li className="nav-item hover-effect">
+                                <NavLink to="/about"
+                                    onClick={() => isActive ? toggleMobileMenu() : ""}
+                                    className={({ isActive }) => isActive ? "underline" : ""}>
+                                    About us
+                                </NavLink>
+                            </li>
+                        </motion.ul>
+
+
+                        <div className="navbar-icons">
+                            <div className="navbar-icon">
+                                <img src="/images/search.svg" alt="" />
+                            </div>
+                            <div className="navbar-icon">
+                                <img src="/images/heart.svg" alt="" />
+                            </div>
                         </div>
                     </div>
-                </div>
-            </nav>
+                </motion.nav>
+            </AnimatePresence>
         </>
     )
 }
