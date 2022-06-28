ac252bec8790b6a29ee8ba3584595a1c564e0c33
Malin Nilsson <malinnilsson87@gmail.com>
2022-06-28 20:26:05 +0200

    Not found page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
     return (
-        <div>NotFound</div>
+        <div className="pages-container">
+            <div className="pages-content not-found">
+                Oops, something went wrong
+            </div>
+
+            <div className="button-container">
+                <Link to="/">
+                    <button className="solid">Go Back</button>
+                </Link>
+            </div>
+        </div>
     )
 }
